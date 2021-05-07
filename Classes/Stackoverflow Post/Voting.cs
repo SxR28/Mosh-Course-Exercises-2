b822ef321168d2f0b266a861b0b162daf9e7e1bc
S✗R <51286027+SorinX006730@users.noreply.github.com>
2021-05-07 18:23:29 +0300

    Add files via upload

---
+++ Classes/Stackoverflow Post/Voting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Stackoverflow_Post
+{
+    class Voting
+    {
+        public List<int> totalUpVotes = new List<int>();
+        public List<int> totalDownVotes = new List<int>();
+        private int upVotes = 0;
+        private int downVotes = 0;
+
+        public int UpVote(int upVote) // Y
+        {
+            this.upVotes = upVote;
+            upVote++;
+            totalUpVotes.Add(upVote);
+
+            return totalUpVotes.Count();
+        }
+
+        public int DownVote(int downVote) // X
+        {
+            this.downVotes = downVote;
+            downVote++;
+            totalDownVotes.Add(downVote);
+
+            return totalDownVotes.Count();
+        }
+    }
+}
---
+++ Classes/StopWatch/V.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert for how much time the timer should run : ");
+            int timerRunningTime = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Start the timer : \n - yes \n - no ");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.Write("Insert your option : ");
+            string timerOption = Console.ReadLine();
+             
+
+            switch(timerOption)
+            {
+                case "yes":
+                    var StartClass = new Start(timerRunningTime, false);
+                    break;
+
+                case "no":
+                    Environment.Exit(0);
+                    break;
+            }
+
+ 
+        }
+    }
+}
