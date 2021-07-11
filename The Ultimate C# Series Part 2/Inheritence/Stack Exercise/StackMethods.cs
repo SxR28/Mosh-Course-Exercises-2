9ded989816c1c750cbcfa76dc0f9d7f9f5071064
Sorin <51286027+SxR28@users.noreply.github.com>
2021-07-11 14:47:46 +0300

    Add files via upload

---
+++ The Ultimate C# Series Part 2/Inheritence/Stack Exercise/StackMethods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackMethods
+    {
+        public void Clear(Stack<object> stack)
+        {
+            Console.WriteLine($"Number of elements before clearing the stack : {stack.Count}");
+
+            Console.WriteLine("Elements cleared [ ! ]");
+            stack.Clear();
+
+            Console.WriteLine($"Number of elements befefore clearing the stack : {stack.Count}");
+        }
+
+        public void Push(object objToPush, int size, Stack<object> stack)
+        {
+                 if (isNull(objToPush) == true)
+                 {
+
+                        Console.WriteLine($"{objToPush} was pushed to the stack");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        stack.Push(objToPush);
+                 }
+                 else
+                    throw new InvalidOperationException("The object you want to push is null");
+            
+       
+        }
+
+        public void Pop(int popLength, Stack<object> stack)
+        {
+            if (isEmpty(stack) == true)
+                throw new InvalidOperationException("Stack is 0");
+            else
+            {
+                for (int i = 0; i < popLength; i++)
+                {
+                    Console.WriteLine(stack.Pop());
+                }
+            }
+        }
+
+        private bool isEmpty(Stack<object> stack)
+        {
+            bool isZero;
+
+            if (stack.Count == 0)
+                isZero = true;
+            else
+                isZero = false;
+
+            return isZero;
+        }
+
+        private bool isNull(object obj)
+        {
+            if (obj == null)
+                return obj is null;
+            else
+                return obj is not null;
+        }
+    }
+}
